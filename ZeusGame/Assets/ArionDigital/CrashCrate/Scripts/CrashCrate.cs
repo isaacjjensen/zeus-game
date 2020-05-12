6b45cbd68fed383ec33a446db771d1c532013200
isaacjjensen <44240610+isaacjjensen@users.noreply.github.com>
2020-05-12 06:00:24 -0500

    Mashed potatoes

---
+++ ZeusGame/Assets/ArionDigital/CrashCrate/Scripts/CrashCrate.cs
@@ -0,0 +1,40 @@
+﻿// This script is modified from the original 
+namespace ArionDigital
+{
+    using UnityEngine;
+
+    public class CrashCrate : Breakable
+    {
+        public GameObject spawnObject;
+        public Vector3 spawnOffset = new Vector3(0, 0, 0);
+
+        [Header("Whole Create")]
+        public MeshRenderer wholeCrate;
+        public BoxCollider boxCollider;
+        [Header("Fractured Create")]
+        public GameObject fracturedCrate;
+        [Header("Audio")]
+        public AudioSource crashAudioClip;
+
+        override public void Break()
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+            crashAudioClip.Play();
+
+            if (spawnObject != null)
+            {
+                Instantiate(spawnObject, gameObject.transform.position + spawnOffset, Quaternion.identity);
+            }
+        }
+
+        [ContextMenu("Test")]
+        public void Test()
+        {
+            wholeCrate.enabled = false;
+            boxCollider.enabled = false;
+            fracturedCrate.SetActive(true);
+        }
+    }
+}
