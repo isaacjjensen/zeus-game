3eecfede0e58454105f4f67615110a9c085aa41b
Parker Combs <41436545+parkerirl@users.noreply.github.com>
2020-05-12 00:28:30 -0500

    changed level 4 collectible to get rid of rigidbody, added water zones to level 4, added electric force field sounds

---
+++ ZeusGame/Assets/turnOffWater.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnOffWater : MonoBehaviour
+{
+    public GameObject underwaterEffect;
+    public GameObject underwaterEffectHorizon;
+    public GameObject underwaterEffectParticles;
+    public GameObject underwaterEffectPostProcessing;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Turns off water effect when passing through force field
+    private void OnTriggerEnter(Collider other)
+    {
+        underwaterEffect.SetActive(false);
+        underwaterEffectHorizon.SetActive(false);
+        underwaterEffectParticles.SetActive(false);
+        underwaterEffectPostProcessing.SetActive(false);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        underwaterEffect.SetActive(false);
+        underwaterEffectHorizon.SetActive(false);
+        underwaterEffectParticles.SetActive(false);
+        underwaterEffectPostProcessing.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        underwaterEffect.SetActive(true);
+        underwaterEffectHorizon.SetActive(true);
+        underwaterEffectParticles.SetActive(true);
+        underwaterEffectPostProcessing.SetActive(true);
+    }
+}
